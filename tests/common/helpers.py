f264e3372b834a5e3ccbc00524feff7fe4b71a5c
Jin XU <jin.xu.2010@gmail.com>
2020-09-23 09:56:43 -0700

    Add files via upload

--- tests/common/helpers.py
+++ tests/common/helpers.py
@@ -7,7 +7,10 @@ import os
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_PATH = os.path.join(DIR, "test_file.xml")
-
+TEST_PATH_1 = os.path.join(DIR, "BIOMD0000000006.xml")
 
 def getSimple():
   return SimpleSBML(TEST_PATH)
+
+def getSimple_BIOMD6():
+  return SimpleSBML(TEST_PATH_1)
