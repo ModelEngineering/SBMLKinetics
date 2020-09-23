c105413caf3c8eaac9d178aced764ac00fd8708a
Jin XU <jin.xu.2010@gmail.com>
2020-09-23 09:55:37 -0700

    Add files via upload

--- tests/common/test_kinetic_law.py
+++ tests/common/test_kinetic_law.py
@@ -21,17 +21,36 @@ IGNORE_TEST = False
 class TestKineticLaw(unittest.TestCase):
 
   def setUp(self):
-    self.simple = helpers.getSimple()
-    self.law = self.simple.reactions[1].kinetic_law
+    #self.simple = helpers.getSimple()
+    self.simple = helpers.getSimple_BIOMD6()
+    global law 
+    law = []
+    for i in range(3):
+      law.append(self.simple.reactions[i].kinetic_law)
 
   def testConstructor(self):
     if IGNORE_TEST:
       return
     self.assertTrue(
-        isinstance(self.law, KineticLaw))
-    trues = [isinstance(s, str) for s in self.law.symbols]
+         isinstance(law[1], KineticLaw))
+    trues = [isinstance(s, str) for s in law[1].symbols]
     self.assertTrue(all(trues))
+
+  def testSymbol(self):
+    def checkSubset(subset,superset):
+      true = set(subset).issubset(set(superset))
+      self.assertTrue(true)
+
+    # const only
+    checkSubset(['kappa'],law[0].symbols)
+
+    # simple kinetic law k*A
+    checkSubset(['k6','u'],law[1].symbols)
+
+    # complex kinetic law with function pow()
+    checkSubset(['z','u','k4','k4prime'],law[2].symbols)
 
 
 if __name__ == '__main__':
   unittest.main()
+
