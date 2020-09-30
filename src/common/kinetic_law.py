c238305a67468615e3bf9981a4fd8063e8c24e7c
Jin XU <jin.xu.2010@gmail.com>
2020-09-30 09:32:03 -0700

    Add files via upload

--- src/common/kinetic_law.py
+++ src/common/kinetic_law.py
@@ -26,7 +26,7 @@ class KineticLaw(object):
     # Reaction for the kinetics law
     self.reaction = reaction
 
-  def __repr__(self):
+  def __repr__(slf):
     return self.formula
 
   def _getSymbols(self):
@@ -51,10 +51,45 @@ class KineticLaw(object):
           additions = augment(child_node, result)
           result.extend(additions)
         else:
-          result.append(child_node.getName())
+          if child_node.isFunction():
+            additions = augment(child_node, result)
+            result.extend(additions)
+          else:
+            result.append(child_node.getName())
       return result
-    # 
+
     ast_node = self.libsbml_kinetics.getMath()
-    result = []
+    if ast_node.getName() is None:
+      result = []
+    else:
+      result = [ast_node.getName()]
     return augment(ast_node, result)
-    return result
+
+  #def _getSymbols(self):
+  #  """
+  #  Finds the parameters and species names for the
+  #  kinetics law. Exposing this information requires
+  #  a recursive search of the parse tree for the
+  #  kinetics expression.
+  #  :return list-str:
+  #  """
+  #  global cur_depth
+  #  MAX_DEPTH = 20
+  #  cur_depth = 0
+  #  def augment(ast_node, result):
+  #    global cur_depth
+  #    cur_depth += 1
+  #    if cur_depth > MAX_DEPTH:
+  #      raise exceptions.BadKineticsMath(self.reaction.id)
+  #    for idx in range(ast_node.getNumChildren()):
+  #      child_node = ast_node.getChild(idx)
+  #      if child_node.getName() is None:
+  #        additions = augment(child_node, result)
+  #        result.extend(additions)
+  #      else:
+  #        result.append(child_node.getName())
+  #    return result 
+  #  ast_node = self.libsbml_kinetics.getMath()
+  #  result = []
+  #  return augment(ast_node, result)
+  #  return result
