48b1f418a7f8afbe35deae961f4a3137a49741ec
Jin <jin.xu.2010@gmail.com>
2021-10-04 10:50:50 -0700

    tests_column_nan_rowExist_type

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,6 +16,7 @@ import unittest
 
 import sys, os 
 import pandas as pd
+import math
 
 IGNORE_TEST = False
 #sys.stdout = open(os.devnull, 'w') #try to block the print from the main() function
@@ -25,6 +26,27 @@ IGNORE_TEST = False
 #############################
 class TestKineticsClassification(unittest.TestCase):
   def testClassification1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (df_classification, _, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    test = all(item in df_classification.columns for item in kinetics_classification.COLUMN_NAME_df_classification)
+    self.assertTrue(test)
+
+  def testClassification2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (df_classification, _, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    self.assertTrue(len(df_classification.index)>0) 
+
+  def testClassification3(self):
     # Test all the elements of df_classification are lists of strings
     if IGNORE_TEST:
       return    
@@ -37,27 +59,29 @@ class TestKineticsClassification(unittest.TestCase):
       list_classification += df_classification.iloc[:,i].tolist()
     test = all(isinstance(item, str) for item in list_classification)
     self.assertTrue(test)
-    COLUMN_NAMES = ["classification name"]
-    trues = [c in df_classification.columns c in COLUMN_NAMES]
-    self.assertTrue(all(trues))
-    # Check that dataframe has no nan values
-    # Check that dataframe has at least 1 row
-
-  # def testClassification2(self):
-  #   # Test all the elements of df_classification are lists of strings
-  #   if IGNORE_TEST:
-  #     return    
-  #   initial_model_indx = 5
-  #   final_model_indx = 6
-  #   #check for biomodel6
-  #   (df_classification, _, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
-  #   list_classification = []
-  #   for i in range(len(df_classification.columns)):
-  #     list_classification += df_classification.iloc[:,i].tolist()
-  #   test = all(isinstance(item, str) for item in list_classification)
-  #   self.assertTrue(test)
 
   def testGenStat1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, df_gen_stat, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    test = all(item in df_gen_stat.columns for item in kinetics_classification.COLUMN_NAME_df_gen_stat)
+    self.assertTrue(test)
+
+  def testGenStat2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, df_gen_stat, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    self.assertTrue(len(df_gen_stat.index)>0) 
+
+  def testGenStat3(self):
     # Test column 'Classification Names' of df_gen_stat is a list of string
     if IGNORE_TEST:
       return    
@@ -65,11 +89,11 @@ class TestKineticsClassification(unittest.TestCase):
     final_model_indx = 6
     #check for biomodel6
     (_, df_gen_stat, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
-    list_gen_stat_classification = df_gen_stat['Classification Names'].tolist()
+    list_gen_stat_classification = df_gen_stat[kinetics_classification.COLUMN_NAME_df_gen_stat[0]].tolist()
     test = all(isinstance(item, str) for item in list_gen_stat_classification)
     self.assertTrue(test) 
 
-  def testGenStat2(self):
+  def testGenStat4(self):
     # Test column 'Percentage' of df_gen_stat is a list of floating numbers
     if IGNORE_TEST:
       return    
@@ -77,11 +101,44 @@ class TestKineticsClassification(unittest.TestCase):
     final_model_indx = 6
     #check for biomodel6
     (_, df_gen_stat, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
-    list_gen_stat_percentage = df_gen_stat['Percentage'].tolist()
+    list_gen_stat_percentage = df_gen_stat[kinetics_classification.COLUMN_NAME_df_gen_stat[1]].tolist()
     test = all(isinstance(item, float) for item in list_gen_stat_percentage)
     self.assertTrue(test) 
 
+  def testGenStat5(self):
+    # Test column 'Percentage' of df_gen_stat does not have nan values
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, df_gen_stat, _) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    list_gen_stat_percentage = df_gen_stat[kinetics_classification.COLUMN_NAME_df_gen_stat[1]].tolist()
+    test = any(math.isnan(item) for item in list_gen_stat_percentage)
+    self.assertFalse(test)
+
   def testMolStat1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, _ , df_mol_stat) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    test = all(item in df_mol_stat.columns for item in kinetics_classification.COLUMN_NAME_df_mol_stat)
+    self.assertTrue(test)
+ 
+  def testMolStat2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, _, df_mol_stat) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    self.assertTrue(len(df_mol_stat.index)>0) 
+
+  def testMolStat3(self):
     # Test column 'SBMLid' of df_mol_stat is a list of string
     if IGNORE_TEST:
       return    
@@ -89,11 +146,11 @@ class TestKineticsClassification(unittest.TestCase):
     final_model_indx = 6
     #check for biomodel6
     (_, _, df_mol_stat) = kinetics_classification.main(initial_model_indx, final_model_indx)
-    list_gen_stat_classification = df_mol_stat['SBMLid'].tolist()
+    list_gen_stat_classification = df_mol_stat[kinetics_classification.COLUMN_NAME_df_mol_stat[0]].tolist()
     test = all(isinstance(item, str) for item in list_gen_stat_classification)
     self.assertTrue(test)
 
-  def testMolStat2(self):
+  def testMolStat4(self):
     # Test column 'Reaction#' of df_mol_stat is a list of integers
     if IGNORE_TEST:
       return    
@@ -101,11 +158,11 @@ class TestKineticsClassification(unittest.TestCase):
     final_model_indx = 6
     #check for biomodel6
     (_, _, df_mol_stat) = kinetics_classification.main(initial_model_indx, final_model_indx)
-    list_gen_stat_classification = df_mol_stat['Reaction#'].tolist()
+    list_gen_stat_classification = df_mol_stat[kinetics_classification.COLUMN_NAME_df_mol_stat[1]].tolist()
     test = all(isinstance(item, int) for item in list_gen_stat_classification)
     self.assertTrue(test)
 
-  def testMolStat3(self):
+  def testMolStat5(self):
     # Test columns other than 'SBMLid' and 'Reaction#' of df_mol_stat are lists of floating numbers
     if IGNORE_TEST:
       return    
@@ -119,7 +176,21 @@ class TestKineticsClassification(unittest.TestCase):
     test = all(isinstance(item, float) for item in list_gen_stat_others)
     self.assertTrue(test)
 
-  def testMolStat4(self):
+  def testMolStat6(self):
+    # Test whether all the numbers are not nan values
+    if IGNORE_TEST:
+      return    
+    initial_model_indx = 5
+    final_model_indx = 6
+    #check for biomodel6
+    (_, _, df_mol_stat) = kinetics_classification.main(initial_model_indx, final_model_indx)
+    list_gen_stat_others = []
+    for i in range(1,len(df_mol_stat.columns)):
+      list_gen_stat_others += df_mol_stat.iloc[:,i].tolist()
+    test = any(math.isnan(item) for item in list_gen_stat_others)
+    self.assertFalse(test)
+
+  def testMolStat7(self):
     # Test the sum of percentage of all types for each model in df_mol_stat is always no less than 1
     if IGNORE_TEST:
       return    
