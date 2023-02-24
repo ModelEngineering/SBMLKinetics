b3cd1e695069c9846aae2d2d913cfb253323d795
Jin <jin.xu.2010@gmail.com>
2023-02-23 17:38:29 -0800

    te

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'python-libsbml',
         'pip>20',
         'sympy',
-        'tellurium!=2.2.7',
+        'tellurium<=2.2.4',
         'urllib3',
         'seaborn',
         'xlsxwriter',
